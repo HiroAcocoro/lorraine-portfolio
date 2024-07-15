eaefb26945b6a6442b0d3b31fe51e0a1cdd357bf
Hiroshi Acocoro <57206370+HiroAcocoro@users.noreply.github.com>
2024-07-15 08:18:08 +0800

    feat(WIP): featured project section 3, art done

--- src/components/home-page/feature-projects-section/porject-card/ProjectCard.tsx
+++ src/components/home-page/feature-projects-section/porject-card/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import ClayButton from "@/components/common/clay-button/ClayButton";
 import styles from "../featured-projects-section.module.css";
-import {FC, useEffect, useRef, useState} from "react";
+import {FC, useCallback, useEffect, useRef, useState} from "react";
 import Image from "next/image";
 
 interface IProjectCard {
@@ -16,7 +16,7 @@ interface IProjectCard {
     link: string;
     gradientBgColor: string;
     boxShadow: string;
-    reversed: boolean;
+    isReversed: boolean;
   };
   artAsset: {
     src: string;
@@ -26,12 +26,26 @@ interface IProjectCard {
     top: string;
     left: string;
     animation: string;
+    isGlitched: boolean;
+    glitchedImages?: string[];
   };
 }
 
 const ProjectCard: FC<IProjectCard> = ({content, artAsset}) => {
-  const [inView, setInView] = useState(false);
   const ref = useRef(null);
+  const [inView, setInView] = useState(false);
+  const [glitchedImgSrc, setGlitchedImgSrc] = useState<number>(0);
+
+  const getRandomNumber = () => Math.floor(Math.random() * 3) + 1;
+  const getRandomInterval = () => Math.floor(Math.random() * 3000) + 1000;
+
+  const updateRandomNumber = useCallback(() => {
+    const newRandomNumber = getRandomNumber();
+    setGlitchedImgSrc(newRandomNumber);
+    setTimeout(() => setGlitchedImgSrc(0), 500);
+    const randomInterval = getRandomInterval();
+    setTimeout(updateRandomNumber, randomInterval);
+  }, []);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -56,6 +70,14 @@ const ProjectCard: FC<IProjectCard> = ({content, artAsset}) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (artAsset.isGlitched && inView) {
+      const randomInterval = getRandomInterval();
+      const timer = setTimeout(updateRandomNumber, randomInterval);
+      return () => clearTimeout(timer);
+    }
+  }, [artAsset.isGlitched, inView, updateRandomNumber]);
+
   return (
     <div className={styles.projectContainer}>
       <div
@@ -69,20 +91,46 @@ const ProjectCard: FC<IProjectCard> = ({content, artAsset}) => {
           height={artAsset.h}
           quality={100}
           alt={artAsset.alt}
+          priority
           style={{
             position: "absolute",
             top: artAsset.top,
-            left: content.reversed ? "" : artAsset.left,
-            right: content.reversed ? artAsset.left : "",
+            left: content.isReversed ? "" : artAsset.left,
+            right: content.isReversed ? artAsset.left : "",
             zIndex: 20,
+            visibility:
+              artAsset.isGlitched && glitchedImgSrc !== 0
+                ? "hidden"
+                : "visible",
           }}
         />
+        {artAsset.isGlitched &&
+          artAsset.glitchedImages?.length &&
+          artAsset.glitchedImages.map((src, index) => (
+            <Image
+              key={src}
+              src={src}
+              width={artAsset.w}
+              height={artAsset.h}
+              quality={100}
+              alt={artAsset.alt}
+              priority
+              style={{
+                position: "absolute",
+                top: artAsset.top,
+                left: content.isReversed ? "" : artAsset.left,
+                right: content.isReversed ? artAsset.left : "",
+                zIndex: 20,
+                visibility: glitchedImgSrc === index + 1 ? "visible" : "hidden",
+              }}
+            />
+          ))}
       </div>
 
       <div
         ref={ref}
         className={styles.projectCard}
-        style={{flexDirection: content.reversed ? "row-reverse" : "row"}}
+        style={{flexDirection: content.isReversed ? "row-reverse" : "row"}}
       >
         <div className={styles.projectInfo}>
           <div className={styles.projectCardHeader}>
--- src/constants/featured-projects.ts
+++ src/constants/featured-projects.ts
@@ -12,7 +12,7 @@ export default [
     link: "/case-studies/mma",
     gradientBgColor: "linear-gradient(to bottom, #EAABCA 0%, #E788B0 100%)",
     boxShadow: "rgb(177 100 132 / 65%) 0px 5px 10px",
-    reversed: false,
+    isReversed: false,
     cardImgAsset: {
       src: "/featured-project-art-1.png",
       w: 198,
@@ -21,6 +21,7 @@ export default [
       top: "-100px",
       left: "-80px",
       animation: "animate__slide_in_bl",
+      isGlitched: false,
     },
   },
   {
@@ -36,7 +37,7 @@ export default [
     link: "/case-studies/mma",
     gradientBgColor: "linear-gradient(to bottom, #0094FF 0%, #002BC5 100%)",
     boxShadow: "rgb(42 44 203 / 65%) 0px 5px 10px",
-    reversed: true,
+    isReversed: true,
     cardImgAsset: {
       src: "/featured-project-art-2.png",
       w: 209,
@@ -45,6 +46,37 @@ export default [
       top: "-150px",
       left: "20px",
       animation: "animate__slide_in_skew",
+      isGlitched: false,
+    },
+  },
+  {
+    title: "MMA lawyer website",
+    logo: {
+      logoSrc: "/mma-logo.png",
+      logoW: 128,
+      logoH: 128,
+    },
+    imgSrc: "/mma-art-3.png",
+    content:
+      "A website that provide potential clients with information about the lawyer's services and to facilitate easy contact. It serves as a professional online presence to attract and inform prospective clients.",
+    link: "/case-studies/mma",
+    gradientBgColor: "linear-gradient(to bottom, #F0FDA4 0%, #DEE20B 100%)",
+    boxShadow: "rgb(223 207 60 / 65%) 0px 5px 10px",
+    isReversed: false,
+    cardImgAsset: {
+      src: "/featured-project-art-3.png",
+      w: 300,
+      h: 278,
+      alt: "person-on-phone-img",
+      top: "-240px",
+      left: "20px",
+      animation: "glitch-in",
+      isGlitched: true,
+      glitchedImages: [
+        "/featured-project-art-3-glitched-1.png",
+        "/featured-project-art-3-glitched-2.png",
+        "/featured-project-art-3-glitched-3.png",
+      ],
     },
   },
 ];
